f9a883e7fa28102318312f5dfff0d5d85f6abb1c
erikapeloggia <erikapeloggia@gmail.com>
2023-07-25 11:41:59 -0300

    cipher e index funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,45 @@
 const cipher = {
-  // ...
+  encode: function(key, message) {
+    key = Number(key);
+    if (typeof key !== 'number' || typeof message !== 'string') {
+      throw new TypeError('Somente letras e números');
+    } else {
+      let encryptMessage = '';
+      for (let i = 0; i < message.length; i++){
+        let char = message[i];
+        const limit = 'A'.charCodeAt();
+        if (char.match(/[A-Z]/)){
+          const codeAsc = ((char.charCodeAt() - limit + key) % 26) + limit;
+          char = String.fromCharCode(codeAsc);
+        }
+        encryptMessage += char;
+      }    
+      return encryptMessage;
+    }
+  },
+  
+  decode: function(key, encryptMessage) {
+    key = Number(key);
+    if (typeof key !== 'number' || typeof encryptMessage !== 'string'){
+      throw new TypeError('Somente letras e números');
+    } else {
+      let decryptMessage = '';
+      for (let i = 0; i < encryptMessage.length; i++){
+        let char = encryptMessage[i];
+        const limit = 'A'.charCodeAt();
+        
+        if (char.match(/[A-Z]/)){
+          const codeAsc = ((char.charCodeAt() + limit - key) % 26) + limit;  
+          char = String.fromCharCode(codeAsc);
+        }
+        decryptMessage += char;
+      }
+      return decryptMessage;
+    }
+  }
+
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
+//console.log(cipher);
 
-console.log(cipher);
+const key = document.getElementById('offset');  
+const message = document.getElementById('message'); 
+
+function encryptMessage (){ 
+  const secret = cipher.encode(key.value, message.value); 
+  document.getElementById('secretMessage').innerText = secret; 
+}
+
+const encrypt = document.getElementById('encrypt');
+
+encrypt.addEventListener('click', encryptMessage); 
+
+const decrypt = document.getElementById('decrypt'); 
+
+function decryptMessage (){
+  const decrypted = cipher.decode(key.value, message.value);
+  document.getElementById('secretMessage').innerText = decrypted;
+}
+decrypt.addEventListener('click', decryptMessage);
