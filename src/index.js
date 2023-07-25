e2e14288ad0650c397f4ed584f837008f4a778cc
erikapeloggia <erikapeloggia@gmail.com>
2023-07-25 20:31:36 -0300

    alterações no index.js e no html e css

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 import cipher from './cipher.js';
-//console.log(cipher);
 
 const key = document.getElementById('offset');  
 const message = document.getElementById('message'); 
+
+message.addEventListener("input", function () {
+  const innerMessage = message.value;
+  const upperCase = innerMessage.toUpperCase();
+  message.value = upperCase;
+});
 
 function encryptMessage (){ 
   const secret = cipher.encode(key.value, message.value); 
   document.getElementById('secretMessage').innerText = secret; 
 }
-
 const encrypt = document.getElementById('encrypt');
-
 encrypt.addEventListener('click', encryptMessage); 
 
-const decrypt = document.getElementById('decrypt'); 
 
 function decryptMessage (){
   const decrypted = cipher.decode(key.value, message.value);
   document.getElementById('secretMessage').innerText = decrypted;
 }
+const decrypt = document.getElementById('decrypt');
 decrypt.addEventListener('click', decryptMessage);
+
+
+function copySecret(){
+  document.getElementById("secretMessage").select();
+  document.execCommand("copy");
+  alert("Copiado!");
+  location.reload();
+}
+const copyMessage = document.getElementById("copyMessage");
+copyMessage.addEventListener('click', copySecret);
+
+const reload = document.getElementById("reload")
+function reloadPage(){
+  location.reload();
+}
+reload.addEventListener("click", reloadPage);
